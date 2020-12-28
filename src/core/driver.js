ce5859081ff0dab5e8dd32e0baa40b047d3053ea
Rolf Li <rolf.x.li@gmail.com>
2020-12-27 21:07:07 -0500

    adding documentation

--- src/core/driver.js
+++ src/core/driver.js
@@ -1,57 +1,60 @@
 const checkAPI = require('./checkAPI')
-const databaseService = require('../services/databaseService')
-const config = require('../providers/config')
+const databaseService = require('../services/databaseService');
+const config = require('../providers/config');
 
 // driver class to handle retrieving target APIs, pinging targets, and recording results
 class Driver {
-    rawTargetInformation = []
-    formattedTargetInformation = []
-    responses = []
-    testInformation = config.testInformation
+    rawTargetInformation = [];
+    formattedTargetInformation = [];
+    responses = [];
+    testInformation = config.testInformation;
 
-    CheckAPI = new checkAPI()
-    DatabaseService = new databaseService()
+    CheckAPI = new checkAPI();
+    DatabaseService = new databaseService();
 
     // handler method to control entire process
     async handleProcess() {
-        await this.retrieveAllResponseTimes()
+        await this.retrieveAllResponseTimes();
 
         // set the API targets
-        await this.retrieveAllUrls()
-        this.formatRequest()
-        console.log(this.formattedTargetInformation)
+        await this.retrieveAllUrls();
+        this.formatRequest();
+        console.log(this.formattedTargetInformation);
 
         // execute API requests and update corresponding status on database
-        this.CheckAPI.checkAPIs(this.formattedTargetInformation)
+        this.CheckAPI.checkAPIs(this.formattedTargetInformation);
     }
 
-    // retrieve the list of users
+    // retrieve all users in the database (data MUST be retrieved before return)
     retrieveUsers() {
-        this.DatabaseService.getAllUsers()
+        this.DatabaseService.getAllUsers();
     }
 
+    // retrieve all URLs in the database (data MUST be retrieved before return)
     async retrieveAllUrls() {
-        this.rawTargetInformation = await this.DatabaseService.getAllUrls()
+        this.rawTargetInformation = await this.DatabaseService.getAllUrls();
     }
 
+    // retrieve all projects in the database (data MUST be retrieved before return)
     retrieveAllProjects() {
-        this.DatabaseService.getAllProjects()
+        this.DatabaseService.getAllProjects();
     }
 
+    // retrieve all response times in the database (data MUST be retrieved before return)
     async retrieveAllResponseTimes() {
-        await this.DatabaseService.getAllResponseTimes()
+        await this.DatabaseService.getAllResponseTimes();
     }
 
     // format raw target information
     formatRequest() {
-        var len = this.rawTargetInformation.length
+        var len = this.rawTargetInformation.length;
         for (var i = 0; i < len; ++i) {
-            var info = this.rawTargetInformation[i]
+            var info = this.rawTargetInformation[i];
             this.formattedTargetInformation.push({
                 urlID: info.UrlId,
                 url: info.Link,
                 method: info.RequestType
-            })
+            });
         }
     }
 }
--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -1,70 +1,71 @@
-const Pool = require('pg').Pool
-const config = require('../providers/config')
-const queries = require('../providers/queries')
+const Pool = require('pg').Pool;
+const config = require('../providers/config');
+const queries = require('../providers/queries');
 
 class databaseService {
     // set up the connection to the database
-    connectionString = config.connectionString
+    connectionString = config.connectionString;
     pool = new Pool({
         host: config.databaseInformation.server,
         port: config.databaseInformation.port,
         user: config.databaseInformation.username,
         password: config.databaseInformation.password,
         database: config.databaseInformation.database
-    })
+    });
 
-    // update status of a target following liveness check
+    // update row containing matching UrlID in ResponseTime table
     async updateStatus(resObj) {
         var values = [resObj.responseStart, resObj.responseTime, resObj.urlId]
         
         this.pool.query(queries.post.responseTime, values, (err, res) => {
             if (err) {
-                console.log(err)
+                console.log(err);
             }
         })
     }
 
+    // retrieve all users in the database (data MUST be retrieved before return)
     async getAllUsers() {
-        // must guarantee user list is retrieved before continuing
-        var query = await this.pool.query(queries.get.getAllUsers)
-        console.log(query.rows)
+        var query = await this.pool.query(queries.get.getAllUsers);
+        console.log(query.rows);
     }
 
+    // retrieve all projects in the database (data MUST be retrieved before return)
     async getAllProjects() {
-        // must guarantee project list is retrieved before continuing
-        var query = await this.pool.query(queries.get.getAllProjects)
-        console.log(query.rows)
+        var query = await this.pool.query(queries.get.getAllProjects);
+        console.log(query.rows);
     }
 
+    // retrieve all URLs in the database (data MUST be retrieved before return)
     async getAllUrls() {
-        // must guarantee URL list is retrieved before continuing
         try {
-            var query = await this.pool.query(queries.get.getAllUrls)
+            var query = await this.pool.query(queries.get.getAllUrls);
             if (query) {
                 if (query.rows) {
-                    return query.rows
+                    return query.rows;
                 }
             }
         } catch (err) {
-            console.log('Error retrieving target URLs.')
+            console.log('Error retrieving target URLs.');
         }
         return []
     }
 
+    // retrieve all response times in the database (data MUST be retrieved before return)
     async getAllResponseTimes() {
         try {
-            var query = await this.pool.query(queries.get.getAllResponseTimes)
+            var query = await this.pool.query(queries.get.getAllResponseTimes);
             if (query) {
                 if (query.rows) {
-                    console.log(query.rows)
-                    return query.rows
+                    console.log(query.rows);
+                    return query.rows;
                 }
             } 
         } catch (err) {
-            console.log('Error retrieving response times.')
+            console.log('Error retrieving response times.');
         }
-        return []
+        return [];
     }
 }
 
-module.exports = databaseService
+module.exports = databaseService;
